bb308124651c41d29779ce22f3c7dfee4d575d42
shuto3726 <sss3726mmm@gmail.com>
2021-04-15 01:44:54 +0000

    課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'top' => 'homes#top', as: 'top'
+  resources :books
+  get 'books/:id' => 'books#show', as: 'b'
+  root :to => 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
